51eec6bb8f2c720a85c99219ea5f7149b9dbb446
synabs <synabsai@gmail.com>
2026-06-04 12:31:27 +0300

    Create LandingPage.tsx

---
+++ src/app/components/LandingPage.tsx
@@ -0,0 +1,19 @@
+import HeroSection from './pages/HeroSection'
+import HowItWorks from './pages/HowItWorks'
+// ...
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <HowItWorks />
+      <ShowcaseSection />
+      <FeaturesSection />
+      <PricingSection />
+      <FAQSection />
+      <CTASection />
+      <Footer />
+    </>
+  )
+}
